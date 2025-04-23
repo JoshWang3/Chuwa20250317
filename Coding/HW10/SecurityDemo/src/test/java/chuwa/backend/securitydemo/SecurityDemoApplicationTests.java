a1d4778ff1af52a56b8fbe2662ce5e860749bd46
xiaoranwu <114265110+Xiaoranabc@users.noreply.github.com>
2025-04-23 13:33:22 -0700

    XiaoranWu add HW 10

---
+++ Coding/HW10/SecurityDemo/src/test/java/chuwa/backend/securitydemo/SecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package chuwa.backend.securitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
