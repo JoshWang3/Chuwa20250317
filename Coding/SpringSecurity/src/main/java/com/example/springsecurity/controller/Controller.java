353f9c7658c8377652006303d1792e3206be8345
Jianan Yao <yao.jiana@northeastern.edu>
2025-05-09 00:01:21 -0700

    hw11

---
+++ Coding/SpringSecurity/src/main/java/com/example/springsecurity/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.example.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+  @GetMapping("/secure")
+  public String checkEndPoint(){
+    return "----reach the endpoint over HTTP----";
+  }
+}
